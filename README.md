08e47b45aa6fb02a31be67ec3f99119a7ebfcb26
Ronnie Bjørkelund <ronnie.bjorkelund@gmail.com>
2016-05-19 15:28:22 +0200

    resetting

--- README.md
+++
@@ -1,4 +0,0 @@
-# beinnor.github.io
-## My portfolio
-
-This is just a portfolio I'm building as I play with web development.
--- js/functions.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-    $(".dropbutton").on("click", function(){
-
-        $("nav").toggleClass("dropmenu");
-
-    });
-
-});
